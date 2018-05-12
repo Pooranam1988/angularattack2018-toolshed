d8f958791ebf93679ac5da8e64ed0be381d951fd
DJCrossman <david@crossman.io>
2018-05-11 18:14:14 -0600

    chore: Initial angular project

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('angularattack2018-toolshed App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
