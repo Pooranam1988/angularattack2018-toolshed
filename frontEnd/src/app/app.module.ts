a817c01a5e9e928ca135c5aaff0ea1c32269bc23
DJCrossman <david@crossman.io>
2018-05-11 18:44:25 -0600

    feat: created initial components

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { DetailComponent } from './detail/detail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule
